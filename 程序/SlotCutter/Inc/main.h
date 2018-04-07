2ace378eec2fe63a06b6cb6f94e059ddd7bbfcb2
zhouruikun <915684080@qq.com>
2018-04-07 15:17:49 +0800

    first commit

---
+++ 程序/SlotCutter/Inc/main.h
@@ -0,0 +1,136 @@
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  ** This notice applies to any and all portions of this file
+  * that are not between comment pairs USER CODE BEGIN and
+  * USER CODE END. Other portions of this file, whether 
+  * inserted by the user or by software development tools
+  * are owned by their respective copyright owners.
+  *
+  * COPYRIGHT(c) 2018 STMicroelectronics
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+/* Includes ------------------------------------------------------------------*/
+
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private define ------------------------------------------------------------*/
+
+#define Step_Motor_Plus_Pin GPIO_PIN_13
+#define Step_Motor_Plus_GPIO_Port GPIOC
+#define Saved_Output0_Pin GPIO_PIN_14
+#define Saved_Output0_GPIO_Port GPIOC
+#define Saved_Output1_Pin GPIO_PIN_15
+#define Saved_Output1_GPIO_Port GPIOC
+#define Material_R_Push_Pin GPIO_PIN_0
+#define Material_R_Push_GPIO_Port GPIOA
+#define X_CS_Pin GPIO_PIN_1
+#define X_CS_GPIO_Port GPIOA
+#define X_UD_Pin GPIO_PIN_2
+#define X_UD_GPIO_Port GPIOA
+#define X_INC_Pin GPIO_PIN_3
+#define X_INC_GPIO_Port GPIOA
+#define Footer_Pull_Pin GPIO_PIN_4
+#define Footer_Pull_GPIO_Port GPIOA
+#define Footer_Push_Pin GPIO_PIN_5
+#define Footer_Push_GPIO_Port GPIOA
+#define Cutter_Motor_Pin GPIO_PIN_0
+#define Cutter_Motor_GPIO_Port GPIOB
+#define Cutter_Pull_Pin GPIO_PIN_1
+#define Cutter_Pull_GPIO_Port GPIOB
+#define Cutter_Push_Pin GPIO_PIN_10
+#define Cutter_Push_GPIO_Port GPIOB
+#define Step_Motor_Dir_Pin GPIO_PIN_11
+#define Step_Motor_Dir_GPIO_Port GPIOB
+#define CCD_Input_Pin GPIO_PIN_12
+#define CCD_Input_GPIO_Port GPIOB
+#define TM1628_DIO_Pin GPIO_PIN_13
+#define TM1628_DIO_GPIO_Port GPIOB
+#define TM1628_CLK_Pin GPIO_PIN_14
+#define TM1628_CLK_GPIO_Port GPIOB
+#define TM1628_STB_Pin GPIO_PIN_15
+#define TM1628_STB_GPIO_Port GPIOB
+#define Cutter_Sensor_Start_Pin GPIO_PIN_8
+#define Cutter_Sensor_Start_GPIO_Port GPIOA
+#define Cutter_Sensor_Mid_Pin GPIO_PIN_9
+#define Cutter_Sensor_Mid_GPIO_Port GPIOA
+#define Cutter_Sensor_End_Pin GPIO_PIN_10
+#define Cutter_Sensor_End_GPIO_Port GPIOA
+#define Footer_Sensor_Start_Pin GPIO_PIN_11
+#define Footer_Sensor_Start_GPIO_Port GPIOA
+#define Footer_Sensor_Mid_Pin GPIO_PIN_12
+#define Footer_Sensor_Mid_GPIO_Port GPIOA
+#define Footer_Sensor_End_Pin GPIO_PIN_15
+#define Footer_Sensor_End_GPIO_Port GPIOA
+#define Material_R_Sensor_Start_Pin GPIO_PIN_3
+#define Material_R_Sensor_Start_GPIO_Port GPIOB
+#define Material_R_Sensor_End_Pin GPIO_PIN_4
+#define Material_R_Sensor_End_GPIO_Port GPIOB
+#define Ket_Start_Pin GPIO_PIN_5
+#define Ket_Start_GPIO_Port GPIOB
+#define Ket_Stop_Pin GPIO_PIN_6
+#define Ket_Stop_GPIO_Port GPIOB
+#define Ket_Shutdown_Pin GPIO_PIN_7
+#define Ket_Shutdown_GPIO_Port GPIOB
+#define Saved_Input0_Pin GPIO_PIN_8
+#define Saved_Input0_GPIO_Port GPIOB
+#define Saved_Input1_Pin GPIO_PIN_9
+#define Saved_Input1_GPIO_Port GPIOB
+
+/* ########################## Assert Selection ############################## */
+/**
+  * @brief Uncomment the line below to expanse the "assert_param" macro in the 
+  *        HAL drivers code
+  */
+/* #define USE_FULL_ASSERT    1U */
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+void _Error_Handler(char *, int);
+
+#define Error_Handler() _Error_Handler(__FILE__, __LINE__)
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H__ */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
